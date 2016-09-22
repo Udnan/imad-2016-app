14db86cb60f289154e08ca5b7cc178794488276d
Udnan <nandukrishnan55555@gmail.com>
2016-09-22 20:11:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,15 @@
 console.log('Loaded!');
 var comment_button=document.getElementById("comment_button");
 comment_button.onclick=function(){
-    
     var comment_box=document.getElementById("comment_box");
-    comment_box.style.visibility="visible";
+    if (comment_button.innerHTML==="+"){
+        comment_button.innerHTML="-"
+        comment_box.style.visibility="visible";
+    }
+    else{
+        comment_button.innerHTML="+"
+        comment_box.style.visibility="hidden";
+    }
+    
+    
 }
