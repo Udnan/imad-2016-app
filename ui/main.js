e7226d0f797c64d890c770a521f9304fc544287b
Udnan <nandukrishnan55555@gmail.com>
2016-09-29 21:08:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,5 +11,20 @@ comment_button.onclick=function(){
         comment_box.style.visibility="hidden";
     }
     
-    
+}
+
+var btnSubmit=document.getElementById("btnSubmit");
+btnSubmit.onclick=funtion(){
+    //get the comments
+    var request= new XMLHttpRequest();
+    request.onReadyState=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if (request.status===200){
+                var counter=request.responseText;
+                console.log(counter)
+            }
+        }
+    }
+    //make request 
+    request.open('GET',"http://udnan.imad.hasura-app.io/comments")
 }
