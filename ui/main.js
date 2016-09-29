35a5e44cfad16158e93458d6282e3592a4efc677
Udnan <nandukrishnan55555@gmail.com>
2016-09-29 23:18:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,8 @@ btnSubmit.onclick=function(){
                 var list='';
                 for(var i=0;i<comments.length;i++){
                     
-                    list='<h5 style:"color:white;">'+comments[i]+'</h5>';
+                    list='<h5>'+comments[i]+'</h5>';
+                    console.log(i);
                     
                 }
                 var divComments=document.getElementById("divComments");
