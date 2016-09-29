dd3fc23f5f126e3bd449616fca0ecf7153047e2c
Udnan <nandukrishnan55555@gmail.com>
2016-09-29 22:55:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,13 +50,6 @@ app.get('/ui/wall.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'wall.jpg'));
 });
 
-var counter=0;
-app.get('/comments',function(req,res){
-    console.log("Accessed comments");
-    counter=counter+1;
-    res.send(counter.toString());
-});
-
 var comments=[];
 app.get('/comments/:comment'),function(req,res){
     var comment=req.params.comment;
@@ -64,6 +57,15 @@ app.get('/comments/:comment'),function(req,res){
     comments.push(comment);
     res.send(JSON.stringify(comments));
 }
+
+var counter=0;
+app.get('/comments',function(req,res){
+    console.log("Accessed comments");
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
