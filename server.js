f0ace92daae3f0db17b677bebe5e5f51f0b0fe03
Udnan <nandukrishnan55555@gmail.com>
2016-11-07 20:03:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 // password handling
 function hash(input,salt){
     var hashed =crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ['pbkdf2sync','10000',salt,hashed.toString('hex')].join('$');
+    return ['pbkdf2S','10000',salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function(req,res){
