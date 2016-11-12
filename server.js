3de1d909a3fb8e3391bc235750a1d9d483c58344
Udnan <nandukrishnan55555@gmail.com>
2016-11-12 14:57:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.get('/login',function(req,res){
 });
 
 app.get('ui/login.js',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','login.js'));
+    res.sendFile(path.join(__dirname,'ui','login.js'));
 });
 
 
