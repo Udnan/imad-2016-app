8f0f3c7ea8cc1c734e0ca8ed74117b5829067396
Udnan <nandukrishnan55555@gmail.com>
2016-11-18 23:51:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,9 +23,48 @@ btnSubmit.onclick=function(){
                 
             }
         }
-    }
+    };
     //make request 
     
     request.open('GET',"http://udnan.imad.hasura-app.io/comments/"+comment_box.value,true);
     request.send(null);
+};
+
+
+// Next article comming soon 
+var span=document.getElementById('text-span');
+var dailog=["Comming Soon...","Waiting For the next article???","I'm working on it ;)"];
+var st="Comming Soon...";
+var count=0;
+var flag=0;
+var no=0;//0 means unfilled or filling 1 means clearing 
+
+function Type() {
+		st=dailog[no];
+		if (flag===0){
+			span.innerHTML = span.innerHTML+st[count];
+    		count+=1;
+		}else{
+			var s= span.innerHTML;
+			span.innerHTML=s.slice(0,s.length-1);
+			count=count-1
+			if (count ===0){
+				flag=0;
+				no=no+1
+				if (no===3){
+					no=0;
+				}
+			}
+		}
+    	
+    	if (count===st.length){
+    		flag=1;
+    	}
+
 }
+
+
+var myVar = setInterval(function(){Type(no);}, 200,no);
+
+
+
