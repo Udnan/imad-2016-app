2705db451f03392785845515054bb378bdef8ae4
Udnan <nandukrishnan55555@gmail.com>
2016-11-20 00:02:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -198,13 +198,6 @@ app.get('/comments/:comment',function(req,res){
 
 var pool=new Pool(config);
 
-/*app.get('/test-db',function(req,res){
-    //goto db
-    pool.query('SELECT * FROM test',function(err,result){if (err){res.status(500).send(err.toString(err));}else{res.send(JSON.strin(result));}});
-});*/
-
-
-
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
